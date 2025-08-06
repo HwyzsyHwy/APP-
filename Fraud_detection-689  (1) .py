e24cc62596a422904f85ae4ffc5d075778aaeda2
HwyzsyHwy <3134403288@qq.com>
2025-08-06 23:51:38 +0800

    Add files via upload

--- Fraud_detection-689  (1) .py
+++ Fraud_detection-689  (1) .py
@@ -1315,7 +1315,7 @@ if st.session_state.current_page == "预测模型":
 
     # 显示当前选择的模型
     st.markdown(f"""
-    <div style="text-align: center; margin-top: 20px; padding: 10px; background: rgba(255,255,255,0.1); border-radius: 10px; backdrop-filter: blur(15px); border: 1px solid rgba(255,255,255,0.0); box-shadow: 0 4px 16px rgba(0,0,0,0.1);">
+    <div style="text-align: center; margin-top: 20px; padding: 10px; background: rgba(255,255,255,0.0); border-radius: 10px; backdrop-filter: blur(15px); border: 1px solid rgba(255,255,255,0.0); box-shadow: 0 4px 16px rgba(0,0,0,0.1);">
         <h4 style="color: white; margin: 0; text-shadow: none;">当前模型：{selected_model}</h4>
     </div>
     """, unsafe_allow_html=True)
